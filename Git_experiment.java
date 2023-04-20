3e711896ad9519cd97b35d476a0a49791d41e67e
Prasad7350 <130764518+Prasad7350@users.noreply.github.com>
2023-04-20 18:20:56 +0530

    Update Git_experiment.java

--- Git_experiment.java
+++ Git_experiment.java
@@ -3,9 +3,12 @@ package Ganesh_lec_march;
 public class Git_experiment {
 	
 	int a=23;
+	int b=45;
+	int d=a+b;
+	
 
 	public static void main(String[] args) {
-		System.out.println("succsesful");
+		System.out.println(d);
 	}
 	}
 
